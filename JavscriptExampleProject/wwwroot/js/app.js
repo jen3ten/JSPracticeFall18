13e881cf8b78de815a0d2e4ff9ec9d5281c70df9
Jen E <jen@wecancodeit.org>
2018-10-25 17:57:36 -0400

    adding ES6 syntax

--- JavscriptExampleProject/wwwroot/js/app.js
+++ JavscriptExampleProject/wwwroot/js/app.js
@@ -1,28 +1,52 @@
-// Make h1 in header have different text
-document.getElementsByTagName('h1')[0].onclick = function () {
-    this.innerText = 'this is different text';
-};
+//// Make h1 in header have different text
+//ES5
+//document.getElementsByTagName('h1')[0].onclick = function () {
+//    this.innerText = 'this is different text';
+//};
 
-// Make nav ul li elements get larger when you hover over them
-var liObj = document.querySelectorAll('nav ul li');
-for (var index in liObj) {
-        liObj[index].onmouseover = function () {
-        this.style.fontSize = '30px';}
+//ES6
+const headerOne = document.getElementsByTagName('h1')[0];
+headerOne.onclick = () => headerOne.innerText = 'this is ES6 text';
+
+//// Make nav ul li elements get larger when you hover over them
+//ES5
+//var liObj = document.querySelectorAll('nav ul li');
+//for (var index in liObj) {
+//        liObj[index].onmouseover = function () {
+//        this.style.fontSize = '30px';}
+//}
+//document.querySelectorAll('nav ul li').forEach(
+//    function (listItem) {
+//        listItem.onmouseleave = function () {
+//            this.style.fontSize = '10px';
+//        }
+//    })
+
+//ES6
+const listObjects = document.querySelectorAll('nav ul li');
+for (let index in listObjects) {
+    listObjects[index].onmouseover = () => listObjects[index].style.fontSize = '30px';
 }
-document.querySelectorAll('nav ul li').forEach(
+
+listObjects.forEach(
     function (listItem) {
-        listItem.onmouseleave = function () {
-            this.style.fontSize = '10px';
-        }
-    })
+        listItem.onmouseleave = () => listItem.style.fontSize = '10px';
+    }
+)
 
-// Add another element to the main element
-var newElement = document.createElement('h3');
-var newText = document.createTextNode('This is a new header');
-newElement.appendChild(newText);
+//// Add another element to the main element
+//ES5
+//var newElement = document.createElement('h3');
+//var newText = document.createTextNode('This is a new header');
+//newElement.appendChild(newText);
 
-var pElement = document.querySelector('main p');
-document.querySelector('main').insertBefore(newElement, pElement);
+//var pElement = document.querySelector('main p');
+//document.querySelector('main').insertBefore(newElement, pElement);
+
+//ES6
+const newMainElement = document.createElement('h3');
+const newMainElementText = document.createTextNode('This is a new ES6 header');
+newMainElement.appendChild(newMainElementText);
 
 // Dynamically change the color of the background on the footer when the user hovers over it
 document.querySelector('footer').onmouseover = function(){
@@ -43,15 +67,17 @@ document.querySelector('footer').onclick = function(){
 }
 
 // Remove the h1 element in the header
-document.querySelector('header h1').remove();
+//ES5
+//document.querySelector('header h1').remove();
 
 // Put something in the place of the h1 in the header
-var headerElem = document.createElement('button');
-var buttonText = document.createTextNode('I\'m new!');
-headerElem.appendChild(buttonText);
+//ES5
+//var headerElem = document.createElement('button');
+//var buttonText = document.createTextNode('I\'m new!');
+//headerElem.appendChild(buttonText);
 
-var navElement = document.querySelector('header nav');
-document.querySelector('header').insertBefore(headerElem, navElement); 
+//var navElement = document.querySelector('header nav');
+//document.querySelector('header').insertBefore(headerElem, navElement); 
 
 // Change the text in one of the paragraph tags in the main element
 document.querySelectorAll('main p')[1].innerText = "Lorem, lorem, blah, blah, blah...";
