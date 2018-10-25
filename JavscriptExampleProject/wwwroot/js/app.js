e4c931300ce54d33fc11b58f06fdfca8fac5f643
Jennifer Evans <jenevans3ten@gmail.com>
2018-10-24 23:52:31 -0400

    finished exercises

--- JavscriptExampleProject/wwwroot/js/app.js
+++ JavscriptExampleProject/wwwroot/js/app.js
@@ -1,7 +1,7 @@
 // Make h1 in header have different text
 document.getElementsByTagName('h1')[0].onclick = function () {
     this.innerText = 'this is different text';
-}
+};
 
 // Make nav ul li elements get larger when you hover over them
 var liObj = document.querySelectorAll('nav ul li');
@@ -17,16 +17,47 @@ document.querySelectorAll('nav ul li').forEach(
     })
 
 // Add another element to the main element
+var newElement = document.createElement('h3');
+var newText = document.createTextNode('This is a new header');
+newElement.appendChild(newText);
+
+var pElement = document.querySelector('main p');
+document.querySelector('main').insertBefore(newElement, pElement);
 
 // Dynamically change the color of the background on the footer when the user hovers over it
+document.querySelector('footer').onmouseover = function(){
+    this.style.backgroundColor = 'red';
+}
 
 // Change the font size of the paragraphs in the main element when the user clicks them
+var pElements = document.querySelectorAll('main p');
+for (var index in pElements){
+    pElements[index].onclick = function (){
+        this.style.fontSize = '2rem';
+    } 
+}
 
 // Change the text in the footer when the user clicks it
+document.querySelector('footer').onclick = function(){
+    document.querySelector('footer p').innerText = 'I changed this';
+}
 
 // Remove the h1 element in the header
+document.querySelector('header h1').remove();
 
 // Put something in the place of the h1 in the header
+var headerElem = document.createElement('button');
+var buttonText = document.createTextNode('I\'m new!');
+headerElem.appendChild(buttonText);
+
+var navElement = document.querySelector('header nav');
+document.querySelector('header').insertBefore(headerElem, navElement); 
+
 // Change the text in one of the paragraph tags in the main element
+document.querySelectorAll('main p')[1].innerText = "Lorem, lorem, blah, blah, blah...";
+
 // Change the size of the header element to 200px using grid syntax
+document.querySelector('header').onclick = function(){
+    document.querySelector('.container').style.gridTemplateRows = '200px';
+}
 
